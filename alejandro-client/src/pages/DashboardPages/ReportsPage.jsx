2a03025239e8d8ae6537caf6ef857aad9705c5e8
kobevcxz <dirkkobe12@gmail.com>
2026-05-05 13:16:24 +0800

    lab-act6

--- alejandro-client/src/pages/DashboardPages/ReportsPage.jsx
+++ alejandro-client/src/pages/DashboardPages/ReportsPage.jsx
@@ -44,18 +44,18 @@ const columns = [
 
 const rows = [
   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 14 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 31 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 33 },
   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 31 },
   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 11 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: 25 },
+  { id: 6, lastName: 'Melisandre', firstName: 'John', age: 22 },
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
 const ReportsPage = () => {
-  const printRef = useRef(null);
+  const printRef = useRef(null)
 
   const handlePrint = () => {
     const printContent = printRef.current;
