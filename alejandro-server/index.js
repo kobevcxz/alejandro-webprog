aea76232ceb95a7b5a101facedde39bdf2da4e37
kobevcxz <dirkkobe12@gmail.com>
2026-06-01 14:28:06 +0800

    fix vercel backend

--- alejandro-server/index.js
+++ alejandro-server/index.js
@@ -1,37 +1,43 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const bodyParser = require("body-parser");
-const jsonParser = bodyParser.json();
+
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const articleRoutes = require("./routes/articleRoutes");
 
 const app = express();
 
-// Database Connection
 connectDB();
 
 app.use(express.json());
-
-// Middleware
-app.use(jsonParser);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// CORS Options
 const corsOptions = {
     origin: "*",
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With"
+    ],
+    methods: [
+        "GET",
+        "HEAD",
+        "PUT",
+        "PATCH",
+        "POST",
+        "DELETE",
+        "OPTIONS"
+    ],
     preflightContinue: false,
     optionsSuccessStatus: 204,
 };
 
 app.use(cors(corsOptions));
 
-// CORS Headers
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
@@ -45,18 +51,16 @@ app.use((req, res, next) => {
     next();
 });
 
-// Routes
+app.get("/", (req, res) => {
+    res.json({ message: "Backend is running!" });
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/articles", articleRoutes);
 
-// Error Handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ message: "Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`)
-);
+module.exports = app;
